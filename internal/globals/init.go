f86781ad95305bd34921527668ec1efa753ca70f
Clive Makamara <cmosh@live.com>
2017-10-03 21:32:38 +0300

    add mpesa public cert and generate security credential

--- internal/globals/init.go
+++ internal/globals/init.go
@@ -1,11 +1,16 @@
 package globals
 
 import (
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/pem"
 	"fmt"
 	"log"
 	"os"
 
 	"github.com/bluele/gcache"
+	"github.com/go-pesa/internal/certs"
 	"github.com/joho/godotenv"
 )
 
@@ -30,6 +35,14 @@ var (
 	Callback string
 	//MSISDN is the MSISDN
 	MSISDN string
+	//Initiator as defined by the docmentation
+	Initiator string
+	//InitiatorShortCode is the shortcode for initiator transactions
+	InitiatorShortCode string
+	//MpesaSecret
+	mpesaSecret string
+	//SecurityCredential encrypted MpesaSecret
+	SecurityCredential string
 )
 
 //InitConfig initializes global variables and all other environment variables
@@ -43,13 +56,16 @@ func InitConfig() {
 	_ = godotenv.Load()
 
 	BaseURL = setEnvironment(os.Getenv("APP_ENV"))
-
 	setVariable(os.Getenv("MPESA_TEST_MSISDN"), &MSISDN, "M-pesa MSISDN")
 	setVariable(os.Getenv("MPESA_CALLBACK_URL"), &Callback, "M-pesa Callback")
 	setVariable(os.Getenv("LIPA_NA_MPESA_PASSKEY"), &PassKey, "M-pesa PassKey")
 	setVariable(os.Getenv("LIPA_NA_MPESA_SHORTCODE"), &ShortCode, "M-pesa ShortCode")
 	setVariable(os.Getenv("MPESA_CONSUMER_KEY"), &ConsumerKey, "M-pesa Consumer Key")
 	setVariable(os.Getenv("MPESA_CONSUMER_SECRET"), &ConsumerSecret, "M-pesa Consumer Secret")
+	setVariable(os.Getenv("MPESA_INITIATOR_CODE_1"), &Initiator, "M-pesa initiator")
+	setVariable(os.Getenv("MPESA_SHORTCODE_1"), &InitiatorShortCode, "M-pesa initiator shortcode")
+	setVariable(os.Getenv("MPESA_SECRET"), &mpesaSecret, "M-pesa secret for security credentials")
+	SecurityCredential = makeSecurityCredential()
 
 }
 
@@ -65,4 +81,25 @@ func setVariable(value string, key *string, variable string) {
 		log.Fatal(fmt.Sprintf("%s not set", variable))
 	}
 	*key = value
+}
+
+func makeSecurityCredential() string {
+	pemData := certs.MpesaCert
+	block, _ := pem.Decode(pemData)
+	if block == nil {
+		log.Fatalf("bad key data: %s", "not PEM-encoded")
+	}
+	if got, want := block.Type, "CERTIFICATE"; got != want {
+		log.Fatalf("unknown key type %q, want %q", got, want)
+	}
+	key, err := x509.ParseCertificate(block.Bytes)
+	if err != nil {
+		log.Fatalf("bad private key: %s", err)
+	}
+	pkey := key.PublicKey.(*rsa.PublicKey)
+	out, err := rsa.EncryptPKCS1v15(rand.Reader, pkey, []byte(mpesaSecret))
+	if err != nil {
+		log.Fatalf("decrypt: %s", err)
+	}
+	return string(out)
 }
---
+++ internal/types/balance.go
@@ -0,0 +1,13 @@
+package types
+
+//BalanceRequest is the response object
+type BalanceRequest struct {
+	Initiator          string `json:"Initiator"`
+	SecurityCredential string `json:"SecurityCredential"`
+	CommandID          string `json:"CommandID"`
+	PartyA             string `json:"PartyA"`
+	IdentifierType     string `json:"IdentifierType"`
+	Remarks            string `json:"Remarks"`
+	QueueTimeOutURL    string `json:"QueueTimeOutURL"`
+	ResultURL          string `json:"ResultURL"`
+}
